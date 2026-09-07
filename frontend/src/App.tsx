88f22fc550f655a28f7f7edf996e7660b378e18e
yashaswinideekonda <yashaswinideekonda12@gmail.com>
2026-09-07 19:57:03 +0530

    Fix deployed login API URL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -427,7 +427,7 @@ function App() {
               onClick={async () => {
   try {
     const response = await fetch(
-      "http://localhost:8000/api/auth/login",
+      "API_BASE + "/api/auth/login"",
       {
         method: "POST",
         headers: {
