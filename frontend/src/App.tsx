da27c9dffd2ca89c958e2ffe2b9a4de33707ccd6
yashaswinideekonda <yashaswinideekonda12@gmail.com>
2026-09-08 16:14:53 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -186,7 +186,7 @@ function App() {
 
     try {
       const response = await fetch(
-        "http://127.0.0.1:8000/api/watchlists/1/stocks?user_id=2",
+        API_BASE + `/api/watchlists/1/stocks?user_id=${userId}`,
         {
           method: "POST",
           headers: {
